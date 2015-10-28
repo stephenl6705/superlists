155619dfc91dc20adebad2fba26557d50a2362bc
Stephen Langestraat <stephen.langestraat@gmail.com>
2015-10-28 22:06:15 +1000

    setting up ExistingListItemForm class with tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,4 @@
+from unittest import skip
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 from django.utils.html import escape
 from django.core.urlresolvers import resolve
@@ -102,6 +103,15 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+        
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
         
         
 class NewListTest(TestCase):
